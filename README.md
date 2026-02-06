6bbc1e8652d18ef7181c6022dcc942e0a681680d
wolf4am <wolf4am@gmail.com>
2026-02-06 16:20:48 +0500

    Added README file

---
+++ README.md
@@ -0,0 +1,2 @@
+This program does not show the real weather.
+It only generates random values based on the city you entered!!
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
 fn main() {
-    println!("Hello, world!");
+    use std::io::{self, Write};
+
+fn main() {
+    println!(" Weather forecast (demo version) ");
+    
+   
+    println!("Enter the name of the city: ");
+    
+    
+  
+    let mut city = String::new();
+    
+  
+    io::stdin()
+        .read_line(&mut city)
+        .expect("Error reading the input");
+    
+   
+    let city = city.trim();
+    
+  
+    if city.is_empty() {
+        println!("You didn't enter the name of the city!");
+        return;
+    }
+  
+    println!("\n--- weather in city {} ---", city);
+    
+    
+    let temp = (city.len() as i32 * 3) % 30 - 5;
+    let conditions = match city.len() % 4 {
+        0 => "☀️  Suny",
+        1 => "☁️  Cloudly",
+        2 => "🌧  Rain",
+        _ => "⛅️ Cloud",
+    };
+    
+    println!("Temperature: {}°C", temp);
+    println!("Conditions: {}", conditions);
+    println!("Humidity: {}%", 40 + (city.len() % 40));
+}
 }
